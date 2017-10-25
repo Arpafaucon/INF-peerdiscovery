2bec886a9c44a950346fc2cc37115fbcfc9a836a
AmaurX <amaury.camus92@gmail.com>
2017-10-25 17:58:57 +0200

    Added first hello constructor

--- src/PeerDiscovery/HelloMessage.java
+++ src/PeerDiscovery/HelloMessage.java
@@ -6,15 +6,49 @@
 package PeerDiscovery;
 
 import java.util.List;
+import java.util.Vector;
 
 /**
  *
  * @author arpaf
  */
 public class HelloMessage {
-	int senderID;
+	String senderID;
 	int sequenceNumber;
 	int HelloInterval;
 	int NumPeers;
-	List<Integer> peers;
+	Vector<String> peers;
+
+	static public String HELLO = "HELLO"; 
+
+	public HelloMessage(String s) throws Exception
+	{
+		String slist[] = s.split(";");
+
+		if(slist.length < 5){
+			throw new Exception("Missing arguments in the hello string");
+		}
+
+		if(!slist[0].equals(HELLO))
+		{
+			throw new Exception("Not a Hello message");
+		}
+
+		senderID = slist[1];
+		sequenceNumber = Integer.parseInt(slist[2]);
+		HelloInterval = Integer.parseInt(slist[3]);
+		NumPeers = Integer.parseInt(slist[4]);
+
+		if(NumPeers != (slist.length - 5))
+		{
+			throw new Exception("Wrong number of peer given...");
+		}
+
+		peers = new Vector<String>();
+
+		for(int i = 5; i < (5 + NumPeers); i++)
+		{
+			peers.add(slist[i]);
+		}
+	}
 }
