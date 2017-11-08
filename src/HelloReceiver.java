fd0bc16acb68425fe65f75d22d81f5e0981d51dd
Arpafaucon <arpafaucon@gmail.com>
2017-11-08 18:23:14 +0100

    up^dated peertable

--- src/HelloReceiver.java
+++ src/HelloReceiver.java
@@ -1,24 +1,41 @@
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author arpaf
  */
-public class HelloReceiver implements SimpleMessageHandler {
+public class HelloReceiver implements SimpleMessageHandler, Runnable {
 
+	private final static int CAPACITY = 10;
 	MuxDemuxSimple mds;
+	BlockingQueue<String> queue = new LinkedBlockingQueue<>(CAPACITY);
 
 	@Override
 	public void handleMessage(String m) {
-		try {
-			HelloMessage hm = new HelloMessage(m);
-			System.out.println("HELLO RECEIVED]\n" + hm.toString());
-		} catch (HelloMessage.HelloException ex) {
-		}
+		queue.add(m);
 	}
 
 	@Override
 	public void setMuxDemux(MuxDemuxSimple md) {
 		mds = md;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				String m = queue.take();
+				HelloMessage hm = new HelloMessage(m);
+				System.out.println("HELLO RECEIVED]\n" + hm.toString());
+			} catch (InterruptedException ex) {
+			} catch (HelloMessage.HelloException ex) {
+			}
+
+		}
 	}
 }
--- src/PeerTable.java
+++ src/PeerTable.java
@@ -1,5 +1,60 @@
 
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
 
+
+/**
+ * PeerTable class
+ * tracks state of all peers, and periodically update them according to messages
+ * or timeouts
+ * @author arpaf
+ */
 public class PeerTable {
+	public enum PeerState{
+		HEARD,
+		INCONSISTENT,
+		SYNCHRONISED,
+		DYING;
+	}
+	
+	public static class PeerRecord{
+		String peerID;
+		InetAddress peerIPAddress;
+		int peerSeqNum;
+		int expirationTime;
+		PeerState peerState;
+	}
+	
+	public class PeerTableUpdater extends Thread{
+		private final int PERIOD;
+		private boolean done = false;
+
+		public PeerTableUpdater(int PERIOD) {
+			this.PERIOD = PERIOD;
+		}
+		
+		@Override
+		public void run() {
+			while(!done){
+				if(this.isInterrupted()){
+					done = true;
+				}
+			}
+		}
+		
+		
+	}
+	
+	private List<PeerRecord> table;
+	private Lock tableLock;
+
+	public PeerTable() {
+		table = new ArrayList<>();
+	}
+	
+	
+	
 	
 }
