5e2e0d8254f00cb7d57c00ec6aa5d0a41abe9c3d
Arpafaucon <arpafaucon@gmail.com>
2017-11-14 16:46:07 +0100

    answered optionnal TD1-Q7 with a copy of HTTP server

---
+++ src/debug/DebugStateMessage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package debug;
+
+/**
+ *
+ * @author arpaf
+ */
+public interface DebugStateMessage {
+	/**
+	 * reads state of the desired value and dumps it as a string
+	 * @return 
+	 */
+	public String readState();
+}
