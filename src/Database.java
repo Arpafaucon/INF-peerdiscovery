e94a24023c33bbe53387e7feacce1ed7c2f60bcb
Arpafaucon <arpafaucon@gmail.com>
2017-11-14 16:46:38 +0100

    first sketch of Database'

---
+++ src/Database.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arpaf
+ */
+public class Database {
+	
+	public static Database internalBase;
+	public static List<Database> peerBase;
+	
+	String data;
+	
+	public List<String> getSplitData(){
+		List<String> res = new ArrayList<>();
+		
+	}
+	
+	
+}
